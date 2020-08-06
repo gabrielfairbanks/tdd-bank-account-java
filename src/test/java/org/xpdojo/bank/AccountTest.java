15b92a29fb074d1c5b0f4bbabf129eb55914c36b
Gabriel Fairbanks <gabriel.fairbanks@gmail.com>
2020-08-06 11:46:18 +0100

    Refactores AccountTest to reduce dupplication

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -1,27 +1,27 @@
 package org.xpdojo.bank;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountTest {
 
+    private final Account account = new Account();
+
     @Test
     public void newAccountsShouldHaveZeroBalance(){
-        Account account = new Account();
         assertThat(account.getBalance()).isEqualTo(0);
     }
 
     @Test
     public void depositAnAmountThatShouldIncreaseTheBalance(){
-        Account account = new Account();
         account.deposit(1);
         assertThat(account.getBalance()).isEqualTo(1);
     }
 
     @Test
     public void multipleDepositsShouldIncreaseTheAmount(){
-        Account account = new Account();
         account.deposit(1);
         account.deposit(1);
         assertThat(account.getBalance()).isEqualTo(2);
@@ -29,18 +29,14 @@ public class AccountTest {
 
     @Test
     public void withdrawalShouldDecreaseTheBalance(){
-        Account account = new Account();
         account.withdraw(5);
         assertThat(account.getBalance()).isEqualTo(-5);
     }
 
     @Test
     public void multipleWithdrawalsShouldDecreaseTheAmount(){
-        Account account = new Account();
         account.withdraw(5);
         account.withdraw(3);
         assertThat(account.getBalance()).isEqualTo(-8);
     }
-
-
 }
