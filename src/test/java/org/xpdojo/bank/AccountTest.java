675e0f539448344f71644ecd9aaf93744e4366bd
Gabriel Fairbanks <gabriel.fairbanks@gmail.com>
2020-08-06 11:03:15 +0100

    Initial test

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -1,6 +1,5 @@
 package org.xpdojo.bank;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,8 +7,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AccountTest {
 
     @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+    public void newAccountsShouldHaveZeroBalance(){
+        Account account = new Account();
+
+        assertThat(account.getBalance()).isEqualTo(0);
     }
+
+
 }
